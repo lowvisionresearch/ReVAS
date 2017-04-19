371ca3a86983d512d930b31954d1c2a2d6a547e6
mehmet <mehmet@85f978e3-65e0-4268-91e7-24df59ab7ce2>
2017-04-19 22:18:31 +0000

    Styled and documented filterStrips.m properly.. by derek.
    git-svn-id: svn://192.168.0.166/ReVAS@21 85f978e3-65e0-4268-91e7-24df59ab7ce2

--- filterStrips.m
+++ filterStrips.m
@@ -1,51 +1,112 @@
-function [filteredStripIndices] = filterStrips(stripIndices)
+function [filteredStripIndices] = FilterStrips(stripIndices)
+% FilterStrips     Replace all NaNs in a column vector with interpolation
+%   FilterStrips(stripIndices) takes a column vector and interpolates
+%   between the values "bordering" each strip of consecutive NaNs. Then it
+%   returns the new vector.
+%   
+%   stripIndices should should have dimensions Nx1.
+%
+%   Example: 
+%       A = [1; 2; 3; 4; 5; NaN; NaN; NaN; NaN; 10];
+%       x = FilterStrips(A);
+%       x(8)
+%       ans = 
+%            8
 
 % get indices of all NaN values
-indices = find(isnan(stripIndices));
-indicesCopy = indices;
+NaNIndices = find(isnan(stripIndices));
+NaNIndicesCopy = NaNIndices;
 
+% Every 3 items in startAndEndPairs will be, in this order: last number
+% before a strip of consecutive NaNs, first number after a strip of NaNs,
+% and number of NaNs in that strip (k). If there is no number after tha NaNs
+% (i.e., the NaNs go to the end of the matrix) then there will only be two
+% numbers: the last number before the NaNs begin and the number of NaNs
 startAndEndPairs = [];
 k = 1;
 
 % find the values that "border" the next segment of consecutive NaNs
-while ~isempty(indicesCopy)
-    dimensions = size(indicesCopy);
+while ~isempty(NaNIndicesCopy)
+    
+    dimensions = size(NaNIndicesCopy);
+    % If there is one item left in the NaNIndicesCopy, that means there is
+    % only one NaN value left. Check if there are more items in the list.
+    % If there are, then we proceed with the "bordering" technique we do
+    % with the rest of the strips of consecutive NaNs. If not, then just
+    % put two values in (last number before NaNs, number of NaNs).
     if dimensions(1) == 1
-        lastBeforeNaN = stripIndices(indices(1) - 1);
-        NaNStrip = [lastBeforeNaN k];
-        startAndEndPairs = [startAndEndPairs NaNStrip];
-        indicesCopy = [];
-    elseif indicesCopy(1) == indicesCopy(2) - 1 
+        lastBeforeNaN = stripIndices(NaNIndices(1) - 1);
+        if (NaNIndicesCopy(1) + 1) <= max(size(stripIndices))
+            firstAfterNaN = stripIndices(NaNIndicesCopy(1) + 1);
+            NaNStrip = [lastBeforeNaN firstAfterNaN k];
+            startAndEndPairs = [startAndEndPairs NaNStrip];
+            NaNIndicesCopy = [];
+        else
+            NaNStrip = [lastBeforeNaN k];
+            startAndEndPairs = [startAndEndPairs NaNStrip];
+            NaNIndicesCopy = [];
+        end
+
+    % If the next index in NaNIndicesCopy is one larger than the current
+    % index, then those indices are part of the same strip of consecutive
+    % NaN values. If that is the case, increment k (number of NaNs in the
+    % strip) and move down NaNIndicesCopy to check the next two indices
+    elseif NaNIndicesCopy(1) == NaNIndicesCopy(2) - 1 
         k = k + 1;
-        indicesCopy = indicesCopy(2:end);
+        NaNIndicesCopy = NaNIndicesCopy(2:end);
+        
+    % If the next index in NaNIndicesCopy is NOT one larger than the
+    % current index, that means that those indices are NOT part of the same
+    % strip of consecutive NaNs (i.e., indices are 485, 486, 487, 510,
+    % 511). In that case, NaNIndices(1)-1 will be the index of the last
+    % number before the NaN strip begins, and NaNIndicesCopy(1) + 1 will be
+    % the index of the first number after the NaN strip ends.
     else
-        lastBeforeNaN = stripIndices(indices(1) - 1);
-        firstAfterNaN = stripIndices(indicesCopy(1) + 1);
+        lastBeforeNaN = stripIndices(NaNIndices(1) - 1);
+        firstAfterNaN = stripIndices(NaNIndicesCopy(1) + 1);
         NaNStrip = [lastBeforeNaN firstAfterNaN k];
         startAndEndPairs = [startAndEndPairs NaNStrip];
-        indices = indices(k+1:end);
+        
+        % Continue moving down the matrices to the values we have not
+        % checked yet.
+        NaNIndices = NaNIndices(k+1:end);
         k = 1;
-        indicesCopy = indicesCopy(2:end);
+        NaNIndicesCopy = NaNIndicesCopy(2:end);
     end
 
 end
 
-% reset the indices variable
-indices = find(isnan(stripIndices));
+% Now that the function has determined the values "bordering" the strips of
+% consecutive NaNs, reset the NaNIndices variable so we can insert
+% interpolated values into the original stripIndices.
+NaNIndices = find(isnan(stripIndices));
 
 while ~isempty(startAndEndPairs)
+    
     dimensions = size(startAndEndPairs);
+    % Remember from earlier that if the NaN strip goes to the end of the
+    % matrix, we only put 2 values into startAndEndPairs (last number
+    % before NaNs begin and number of NaNs in the strip). In that case,
+    % assume the last number before the NaNs remains constant, so set
+    % the rest of the NaN values in the matrix equal to that last number.
     if dimensions(2) == 2
-        stripIndices(indices(1):end) = startAndEndPairs(1);
+        stripIndices(NaNIndices(1):end) = startAndEndPairs(1);
         startAndEndPairs = [];
+        
+    % Remember that startAndEndPairs generally has chunks of 3 values: last
+    % number before NaNs start, first number after NaNs begin, and number
+    % of consecutive NaNs in a strip. Therefore, if we assume the change
+    % in position between the two real values is linear, then dy is simply 
+    % (last number before NaNs - first number after NaNs) / k. Then
+    % replace all the NaNs in the original stripIndices with dy
     else
         start = round(startAndEndPairs(1));
-        ending = round(startAndEndPairs(2));
-        numPoints = startAndEndPairs(3) + 1;
+        ending = round(startAndEndPairs(2)) - 1;
+        numPoints = startAndEndPairs(3);
         dy = (ending-start)/numPoints;
-        for point = start+dy:dy:ending
-            stripIndices(indices(1)) = point;
-            indices = indices(2:end);
+        for point = (start+dy) : dy : ending
+            stripIndices(NaNIndices(1)) = round(point);
+            NaNIndices = NaNIndices(2:end);
         end
     end
     startAndEndPairs = startAndEndPairs(4:end);
