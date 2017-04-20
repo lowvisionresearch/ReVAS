30932af3ed7a3795ec50d4d2943b40b0cd5ccea3
derek <derek@85f978e3-65e0-4268-91e7-24df59ab7ce2>
2017-04-20 18:49:24 +0000

    Added comments for Crop function and added argument for 'margin'
    git-svn-id: svn://192.168.0.166/ReVAS@25 85f978e3-65e0-4268-91e7-24df59ab7ce2

--- Crop.m
+++ Crop.m
@@ -1,24 +1,60 @@
-function [result] = Crop(image, width, height)
+function [result] = Crop(image, width, height, margin)
+% Crop     Crop out 0 padding from an image
+%   Crop(image, width, height, margin) takes in a two-dimensional matrix, its 
+%   width, its height, and a margin size for which it tests for 0's. The
+%   function removes 0 padding, given that there is a region of size 'margin' that
+%   is made up entirely of 0's. 
+%
+%   Example: 
+%       A = [1 2 3 4 5 0 0; 
+%            1 2 3 4 5 0 0; 
+%            0 0 0 0 0 0 0; 
+%            0 0 0 0 0 0 0]
+%       x = Crop(A, 7, 4, 2)
+%       x = 
+%           1 2 3 4 5
+%           1 2 3 4 5
 
+% Get row/column vectors made up of logicals. (1 means there is at least one
+% non-zero value within a row or column. 0 means the entire row or column is 
+% made up of 0's)
 rowBoolean = any(image, 2);
 columnBoolean = any(image, 1);
 
 rowDimensions = size(rowBoolean);
 columnDimensions = size(columnBoolean);
 
-% grab the first row that is entirely 0
 for row = 1:rowDimensions(1) 
-    if rowBoolean(row, 1) == 0
-        break
+    % checking that row+margin won't exceed matrix dimensions
+    if row+margin <= size(rowBoolean(1))
+        % if the margin space is entirely 0's, then save the row number
+        if rowBoolean(row:row+margin, 1) == 0
+            break
+        end
+    else
+        % if row+margin exceeds matrix dimensions, just check if row to the
+        % end of the matrix is made up entirely of 0's
+        if rowBoolean(row:end, 1) == 0
+            break
+        end
     end
 end
 
-% grab the first column that is entirely 0
+% Same idea for columns, from row processing
 for column = 1:columnDimensions(2) 
-    if columnBoolean(1, column) == 0
-        break
+    if column+margin <= size(columnBoolean(2))
+        if columnBoolean(1, column:column+margin) == 0
+            break
+        end
+    else
+        if columnBoolean(1, column:end) == 0
+            break
+        end
     end
 end
+
+% Use saved row/column value as the lower limit and the edge of the matrix
+% as the upper limit to specify the area to be sliced (cropped) out.
 
 image(row:height, :) = [];
 image(:, column:width) = [];
