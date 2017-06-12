9e6c096c1db416f1e57f9899295c65584b535107
matt <matt@85f978e3-65e0-4268-91e7-24df59ab7ce2>
2017-06-12 18:30:32 +0000

    changed to correct bandpass filter file convention, other minor changes.
    git-svn-id: svn://192.168.0.166/ReVAS@37 85f978e3-65e0-4268-91e7-24df59ab7ce2

--- BandpassFilter.m
+++ BandpassFilter.m
@@ -1,21 +1,21 @@
 function BandpassFilter(inputVideoPath, parametersStructure)
 %BANDPASS FILTER Applies bandpass filtering to the video
-%   The result is stored with '_bpfiltered' appended to the input video file
+%   The result is stored with '_bandfilt' appended to the input video file
 %   name.
 %
 %   |parametersStructure.overwrite| determines whether an existing output
 %   file should be overwritten and replaced if it already exists.
 
-outputVideoPath = [inputVideoPath(1:end-4) '_bpfiltered' inputVideoPath(end-3:end)];
+outputVideoPath = [inputVideoPath(1:end-4) '_bandfilt' inputVideoPath(end-3:end)];
 
 %% Handle overwrite scenarios.
 if ~exist(outputVideoPath, 'file')
     % left blank to continue without issuing warning in this case
 elseif ~isfield(parametersStructure, 'overwrite') || ~parametersStructure.overwrite
-    warning('RemoveStimuli() did not execute because it would overwrite existing file.');
+    warning('BandpassFilter() did not execute because it would overwrite existing file.');
     return;
 else
-    warning('RemoveStimuli() is proceeding and overwriting an existing file.');
+    warning('BandpassFilter() is proceeding and overwriting an existing file.');
 end
 
 %% Set bandpassSigma
