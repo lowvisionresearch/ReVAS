1edda54cb6d39d3b9954706387c37f24874d35c3
derek <derek@85f978e3-65e0-4268-91e7-24df59ab7ce2>
2017-06-20 00:34:10 +0000

    Added GPU option and fixed bug in MakeMontage
    git-svn-id: svn://192.168.0.166/ReVAS@44 85f978e3-65e0-4268-91e7-24df59ab7ce2

--- MakeMontage.m
+++ MakeMontage.m
@@ -9,6 +9,7 @@ function [refFrame] = MakeMontage(params, fileName)
 %
 %   Example: 
 %       params.stripHeight = 15;
+%       params.time = 1/540:1/540:1;
 %       params.positions = randn(540, 2);
 %       params.samplingRate = 540;
 %       MakeMontage(params,'fileName');
@@ -41,22 +42,31 @@ stripsPerFrame = round(frameHeight/params.newStripHeight);
 counterArray = zeros(frameHeight*2);
 refFrame = zeros(frameHeight*2);
 
-% creating another video object in order to use each individual frame later
-videoFReader = vision.VideoFileReader(fileName);
+% Negate all positions 
+stripIndices = -stripIndices;
 
-% scale the strip coordinates so that all values are positive
+% Scale the frame coordinates so that all values are positive. Take the
+% negative value with the highest magnitude in each column of
+% framePositions and add that value to all the values in the column. This
+% will zero the most negative value (like taring a weight scale). Then add
+% a positive integer to make sure all values are > 0 (i.e., if we leave the
+% zero'd value in framePositions, there will be indexing problems later,
+% since MatLab indexing starts from 1 not 0).
 mostNegative = max(-1*stripIndices);
 stripIndices(:, 1) = stripIndices(:, 1) + mostNegative(1) + 2;
 stripIndices(:, 2) = stripIndices(:, 2) + mostNegative(2) + 2;
 
-% scaling the time array to accomodate new strip height
-scalingFactor = ((params.stripHeight)/2)/(frameRate*frameHeight);
+% scaling the time array to accomodate new strip height CHANGE TOTALFRAMES
+% TO FRAMERATE MAYBE
+scalingFactor = ((params.stripHeight)/2)/(totalFrames*frameHeight);
 t1 = t1 + scalingFactor;
-dt = params.newStripHeight / (frameRate * frameHeight);
-t2 = 0:dt:videoInfo.Duration + scalingFactor;
+dt = params.newStripHeight / (totalFrames * frameHeight);
+t2 = dt:dt:videoInfo.Duration + scalingFactor;
 
-% transpose t2 because t1 is a column vector
-t2 = t2';
+% Make sure that both time arrays have the same dimensions
+if size(t2, 1) ~= size(t1, 1) && size(t2, 2) ~= size(t1, 2)
+    t2 = t2';
+end
 
 % replace NaNs with a linear interpolation, done manually in a helper
 % function
@@ -64,59 +74,75 @@ filteredStripIndices1 = FilterStrips(stripIndices(:, 1));
 filteredStripIndices2 = FilterStrips(stripIndices(:, 2));
 filteredStripIndices = [filteredStripIndices1 filteredStripIndices2];
 
-% interpolating positions between strips, using pchip
+% interpolating positions between strips, using pchip 
 interpolatedPositions = interp1(t1, filteredStripIndices, t2, 'pchip');
 
 for frameNumber = 1:totalFrames
     
-    videoFrame = step(videoFReader);
+    % Read frame and convert pixel values to signed integers
+    videoFrame = double(readFrame(videoInfo))/255;
     
     % get the appropriate strips from stripIndices for each frame
     n = frameNumber;
     startFrameStrips = round(1 + ((n-1)*(stripsPerFrame)));
     endFrameStrips = round(n * stripsPerFrame);
     
-    % keep track of the strip number, so we can move it vertically
-    % accordingly
-    stripNumber = 1;
+    if endFrameStrips > size(interpolatedPositions, 1)
+        endFrameStrips = size(interpolatedPositions, 1);
+    end
     
+    % keep track of the strip number, so we can move it vertically accordingly
+    stripNumber = 1;
+
     for strip = startFrameStrips : endFrameStrips
-        
         % row and column "coordinates" of the top left pixel of each strip
         topLeft = [interpolatedPositions(strip, 1), interpolatedPositions(strip, 2)];
         columnIndex = round(topLeft(2));
         rowIndex = round(topLeft(1));
         
-        % move strip to proper position 
+        % move strip to proper position
         rowIndex = rowIndex + ((stripNumber-1) * params.newStripHeight);
         
         % get max row/column of the strip
         maxRow = rowIndex + params.newStripHeight - 1;
         maxColumn = columnIndex + w - 1;
         
-        % making sure the row values don't exceed videoFrame dimensions
-        if maxRow > frameHeight
-            maxRow = frameHeight;
-        end
-
         % transfer values of the strip pixels to the reference frame, and
         % increment the corresponding location on the counter array
-        for i = rowIndex : maxRow
-            for j = columnIndex : maxColumn
-                refFrame(i,j) = refFrame(i,j) + videoFrame(i,j);
-                counterArray(i, j) = counterArray(i, j) + 1;
-            end
-        end
+        templateSelectRow = rowIndex:maxRow;
+        templateSelectColumn = columnIndex:maxColumn;
+        vidStart = ((stripNumber-1)*params.newStripHeight)+1;
+        vidEnd = stripNumber * params.newStripHeight;
+        
+        % If the strip extends beyond the frame (i.e., the frame has a
+        % height of 512 pixels and strip of height 10 begins at row 511)
+        % set the max row of that strip to be the last row of the frame.
+        % Also make templateSelectRow smaller to match the dimensions of
+        % the new strip
+         if vidEnd > size(videoFrame, 1)
+             difference = vidEnd - size(videoFrame, 1);
+             vidEnd = size(videoFrame, 1);
+             templateSelectRow = rowIndex:(maxRow-difference);
+         end
+        
+        refFrame(templateSelectRow, templateSelectColumn) = refFrame(...
+            templateSelectRow, templateSelectColumn) + videoFrame(...
+            vidStart:vidEnd, :);
+        counterArray(templateSelectRow, templateSelectColumn) = counterArray...
+            (templateSelectRow, templateSelectColumn) + 1;
 
         % increment stripNumber for the next iteration of the for-loop
         stripNumber = stripNumber + 1;
     end
 end
 
+
 % divide each pixel in refFrame by the number of strips that contain that pixel
 refFrame = refFrame./counterArray;
 
-refFrame = Crop(refFrame, w*2, frameHeight * 2, 100);
+refFrame = Crop(refFrame);
+save('Reference Frame', 'refFrame');
+figure(1)
 imshow(refFrame);
 
 end
---
+++ RefineReferenceFrame.m
@@ -0,0 +1,43 @@
+function refinedFrame = RefineReferenceFrame(coarseRefFrame, params)
+%REFINE REFERENCE FRAME  Generate a better reference frame.
+%   The function alternates between StripAnalysis and MakeMontage,
+%   alternating between generating positions and generating the reference
+%   frames that result from those positions
+%   
+%   params takes in the fields stripHeight, stripWidth, samplingRate,
+%   enableSubpixelInterpolation,subpixelInterpolationParameters.neighborhoodSize, 
+%   subpixelInterpolationParameters.subpixelDepth, adaptiveSearch,
+%   adaptiveSearchScalingFactor,searchWindowHeight, badFrames, minimumPeakRatio,
+%   minimumPeakThreshold, enableVerbosity, axesHandles, enableGPU,
+%   videoPath, numberOfIterations.
+
+% First perform strip analysis on the coarseRefFrame to get a rough
+% estimate of the strip positions
+[~, usefulEyePositionTraces, timeArray, ~] = ...
+    StripAnalysis(params.fileName, coarseRefFrame, params);
+
+% For a certain number of iterations specified by the user, pingpong back
+% and forth between extracting positions and generating reference frames
+% based on those positions
+k = 0;
+while k < params.numberOfIterations
+    params.positions = usefulEyePositionTraces;
+    params.time = timeArray;
+    
+    newRefFrame = MakeMontage(params, params.fileName);
+    
+    % If this is not the last iteration, perform strip analysis using the new
+    % reference frame. If this is the last iteration, do not execute this
+    % suite because the reference frame has already been updated to its
+    % final form.
+    if k ~= params.numberOfIterations - 1
+        [~, usefulEyePositionTraces, timeArray, ~] = ...
+        StripAnalysis(params.fileName, newRefFrame, params);
+    end
+    
+    k = k + 1;
+end
+
+refinedFrame = newRefFrame;
+
+end
