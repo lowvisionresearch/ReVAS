1548d06708e5a5a02a00cc3fb6eaa7df1bbb7b84
derek <derek@85f978e3-65e0-4268-91e7-24df59ab7ce2>
2017-08-11 01:10:46 +0000

    matt updated template_params.mat
    git-svn-id: svn://192.168.0.166/ReVAS@154 85f978e3-65e0-4268-91e7-24df59ab7ce2

--- testbench/BenchmarkingCreateParamFiles.m
+++ testbench/BenchmarkingCreateParamFiles.m
@@ -24,10 +24,32 @@ for i = 1:length(filenames)
     
     nameEnd = strfind(originalVideoPath,'bandfilt');
     paramsPath = [originalVideoPath(1:nameEnd+length('bandfilt')-1) '_params'];
+    
+    % Remember video path to make it more convenient to compare params
+    % files.
     coarseParameters.originalVideoPath = originalVideoPath;
     fineParameters.originalVideoPath = originalVideoPath;
     stripParameters.originalVideoPath = originalVideoPath;
+    
+    % Read the videos
+    videoArray = VideoPathToArray(originalVideoPath);
+    frameHeight = size(videoArray, 1);
+    frameWidth = size(videoArray, 2);
+    
+    % Strip Width
+    fineParameters.stripWidth = frameWidth;
+    stripParameters.stripWidth = frameWidth;
+    
+    % Calculate Sampling Rate based on Strip Height
+    stripsPerFrame = floor(frameHeight/fineParameters.stripHeight);
+    framesPerSecond = 30;
+    fineParameters.samplingRate = stripsPerFrame * framesPerSecond;
+    
+    stripsPerFrame = floor(frameHeight/stripParameters.stripHeight);
+    stripParameters.samplingRate = stripsPerFrame * framesPerSecond;
+    
     save(paramsPath, 'coarseParameters', 'fineParameters', 'stripParameters');
+    fprintf('%d of %d completed.\n', i, length(filenames));
 end
 fprintf('Process Completed.\n');
 
