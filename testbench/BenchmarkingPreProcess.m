70cbbf34d4ff62e6b80522b4a939570745e4b9ff
derek <derek@85f978e3-65e0-4268-91e7-24df59ab7ce2>
2017-08-22 21:54:43 +0000

    matt split off preprocess to separate file
    git-svn-id: svn://192.168.0.166/ReVAS@161 85f978e3-65e0-4268-91e7-24df59ab7ce2

---
+++ testbench/BenchmarkingPreProcess.m
@@ -0,0 +1,82 @@
+function [] = BenchmarkingPreProcess()
+%BENCHMARKING Script used to pre-process.
+%   Script used to benchmark ReVAS.
+
+%%
+clc;
+clear;
+close all;
+
+addpath(genpath('..'));
+
+filenames = uipickfiles;
+if ~iscell(filenames)
+    if filenames == 0
+        fprintf('User cancelled file selection. Silently exiting...\n');
+        return;
+    end
+end
+
+for i = 1:length(filenames)
+    % Grab path out of cell.
+    videoPath = filenames{i};
+    parametersStructure = struct;
+    stimulus = struct;
+    
+    % Overwrite parameter:
+    % if true, recompute and replace existing output file if already present.
+    % if false and output file already exists, abort current function/step and continue.
+    parametersStructure.overwrite = true;
+
+    % Step 1: Trim the video's upper and right edges.
+    parametersStructure.borderTrimAmount = 0;
+    TrimVideo(videoPath, parametersStructure);
+    fprintf('Process Completed for TrimVideo()\n');
+    videoPath = [videoPath(1:end-4) '_dwt' videoPath(end-3:end)]; %#ok<*FXSET>
+
+    % Step 2: Find stimulus location
+    parametersStructure.enableVerbosity = true;
+    %FindStimulusLocations(videoPath, 'testbench/stimulus_cross.gif', parametersStructure);
+    %stimulus.thickness = 1;
+    %stimulus.size = 11;
+    % For Rodenstock:
+    stimulus.thickness = 3;
+    stimulus.size = 23;
+    FindStimulusLocations(videoPath, stimulus, parametersStructure);
+    fprintf('Process Completed for FindStimulusLocations()\n');
+    
+    % Step 3: Remove the stimulus
+    RemoveStimuli(videoPath, parametersStructure);
+    %copyfile(videoPath, ...
+    %    [videoPath(1:end-4) '_nostim' videoPath(end-3:end)]); %#ok<*FXSET>
+    fprintf('Process Completed for RemoveStimuli()\n');
+
+    % Step 4: Apply gamma correction
+    videoPath = [videoPath(1:end-4) '_nostim' videoPath(end-3:end)]; %#ok<*FXSET>
+    parametersStructure.gammaExponent = 0.6;
+    GammaCorrect(videoPath, parametersStructure);
+    fprintf('Process Completed for GammaCorrect()\n');
+
+    % Step 5: Apply bandpass filtering
+    videoPath = [videoPath(1:end-4) '_gamscaled' videoPath(end-3:end)];
+    parametersStructure.smoothing = 1;
+    parametersStructure.lowSpatialFrequencyCutoff = 3;
+    BandpassFilter(videoPath, parametersStructure);
+    fprintf('Process Completed for BandpassFilter()\n');
+        
+    % Step 6: Detect blinks and bad frames
+    % Default:
+    %parametersStructure.thresholdValue = 4;
+    parametersStructure.thresholdValue = 1;
+    parametersStructure.singleTail = true;
+    parametersStructure.upperTail = false;
+    % Use the final bandpass filtered video
+    videoPath = [videoPath(1:end-4) '_bandfilt' videoPath(end-3:end)];
+    FindBlinkFrames(videoPath, parametersStructure);
+    fprintf('Process Completed for FindBadFrames()\n');
+    % FindBlinkFrames still needs file name from before stim removal.
+    
+end
+fprintf('Process Completed.\n');
+end
+
