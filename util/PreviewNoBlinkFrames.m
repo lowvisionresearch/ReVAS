1b0b3cd9f5d1bf474b8a419f5f773226cd7f4fea
matt <matt@85f978e3-65e0-4268-91e7-24df59ab7ce2>
2017-08-29 23:28:55 +0000

    matt wrote PreviewNoBlinkFrames.m
    git-svn-id: svn://192.168.0.166/ReVAS@169 85f978e3-65e0-4268-91e7-24df59ab7ce2

---
+++ util/PreviewNoBlinkFrames.m
@@ -0,0 +1,52 @@
+function PreviewNoBlinkFrames()
+%PREVIEW NO BLINK FRAMES Generates a video with blink frames ommitted
+%   Generates a video with blink frames ommitted. This can be helpful in
+%   determining whether an adequate number of blink frames have been
+%   removed or whether pre-processing should be adjusted.
+
+addpath(genpath('..'));
+
+filenames = uipickfiles;
+if ~iscell(filenames)
+    if filenames == 0
+        fprintf('User cancelled file selection. Silently exiting...\n');
+        return;
+    end
+end
+
+parfor i = 1:length(filenames)
+    originalVideoPath = filenames{i};
+    blinkFramesPath = [originalVideoPath(1:end-4) '_blinkframes'];
+    outputVideoPath = [originalVideoPath(1:end-4) '_noblink' originalVideoPath(end-3:end)];
+
+    load(blinkFramesPath);
+    % badFrames should now be loaded.
+    
+    % Trim the video frame by frame.
+    writer = VideoWriter(outputVideoPath, 'Grayscale AVI');
+    open(writer);
+
+    [videoInputArray, ~] = VideoPathToArray(originalVideoPath);
+
+    height = size(videoInputArray, 1);
+    width = size(videoInputArray, 2);
+    numberOfFrames = size(videoInputArray, 3);
+
+    % Preallocate.
+    result = zeros(height, width, ...
+        numberOfFrames-size(badFrames, 2), 'uint8');
+    
+    resultIndex = 1;
+    for frameNumber = 1:numberOfFrames
+        if ~ismember(frameNumber, badFrames)
+            result(:,:,resultIndex) = videoInputArray(:,:,frameNumber);
+            resultIndex = resultIndex + 1;
+        end
+    end
+    colormap(gray(256));
+
+    writeVideo(writer, result);
+    close(writer);
+end
+fprintf('Process Completed.\n');
+end
