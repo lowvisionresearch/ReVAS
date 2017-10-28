0198f1553dd9b78ebb906c88e7a64c88f43c2123
matt <matt@85f978e3-65e0-4268-91e7-24df59ab7ce2>
2017-10-28 18:37:40 +0000

    matt updated analysis report scripts
    git-svn-id: svn://192.168.0.166/ReVAS@222 85f978e3-65e0-4268-91e7-24df59ab7ce2

--- util/MatPlotToImage.m
+++ util/MatPlotToImage.m
@@ -44,6 +44,31 @@ if ~iscell(filenames)
     end
 end
 
+% Go through list of selected items and filter
+i = 1;
+while i <= size(filenames, 2)
+    if isdir(filenames{i})
+        % Pull out any files contained within a selected folder
+        % Save the path
+        folder = filenames{i};
+        % Delete the path from the list
+        filenames(i) = [];
+        
+        % Append files to our list of files if they match the suffix
+        folderFiles = dir(folder);
+        for j = i:size(folderFiles, 1)
+            if ~strcmp(folderFiles(j).name,'.') && ~strcmp(folderFiles(j).name,'..') && ...
+                    isempty(findstr('.jpg', folderFiles(j).name))
+                filenames = ...
+                    [filenames, {fullfile(folderFiles(j).folder, folderFiles(j).name)}];
+            end
+        end
+    else
+        % Only increment if we did not delete an item this iteration.
+        i = i + 1;
+    end
+end
+
 for i = 1:length(filenames)
     % Grab path out of cell.
     matFilePath = filenames{i};
@@ -54,11 +79,14 @@ for i = 1:length(filenames)
     
     % Convert from pixels to degrees
     eyePositionTraces = eyePositionTraces * degreesPerPixel;
-
     plot(timeArray, eyePositionTraces)
-    title('Raw Eye Position Traces');
-    xlabel('Time (sec)');
-    ylabel('Eye Position Traces (degrees)');
+    box off;
+    title('Eye Position Traces');
+    ax = gca;
+    ax.XAxis.FontSize = 14;
+    ax.YAxis.FontSize = 14;
+    xlabel('Time (sec)','FontSize',18);
+    ylabel('Eye Position Traces (degrees)','FontSize',18);
     legend('show');
     legend('Horizontal Traces', 'Vertical Traces');
     saveas(gcf, jpgFilePath, 'jpg')
