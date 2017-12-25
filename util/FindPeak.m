70123b29d86bbcc8845d09a799ef7a5acc31b23b
Matthew Sit <msit@berkeley.edu>
2017-12-24 19:56:53 -0800

    clean up stripanalysis default variable usage

--- util/FindPeak.m
+++ util/FindPeak.m
@@ -47,6 +47,29 @@ function [xPeak, yPeak, peakValue, secondPeakValue] = ...
 %                               enableGaussianFiltering is true)
 %                               (default 10)
 
+%% Set parameters to defaults if not specified.
+
+if ~isfield(parametersStructure, 'stripHeight')
+    stripHeight = 15;
+    warning('using default parameter for stripHeight');
+else
+    stripHeight = parametersStructure.stripHeight;
+    if ~IsNaturalNumber(stripHeight)
+        error('stripHeight must be a natural number');
+    end
+end
+
+if ~isfield(parametersStructure, 'enableGaussianFiltering')
+    enableGaussianFiltering = false;
+else
+    enableGaussianFiltering = parametersStructure.enableGaussianFiltering;
+    if ~islogical(enableGaussianFiltering)
+        error('enableGaussianFiltering must be a logical');
+    end
+end
+
+%% Find peak
+
 % Cut out smaller correlation map to search in if applicable. Doing this
 % will essentially restrict searching to the center area and any false
 % peaks near the edges will be ignored.
@@ -65,128 +88,127 @@ end
 if isfield(parametersStructure, 'peakDropWindow')
    peakDropWindow = parametersStructure.peakDropWindow;
 else
-   peakDropWindow = parametersStructure.stripHeight;
+   peakDropWindow = stripHeight;
 end
 
 % Use the appropriate method to identify the peak
-    if parametersStructure.enableGaussianFiltering
-        % Apply Gaussian Filter and get difference between correlations
-        gaussianFilteredCorrelation = ...
-            imgaussfilt(correlationMap, ...
-            parametersStructure.gaussianStandardDeviation);
-        gaussianDifferenceCorrelation = ...
-            correlationMap - gaussianFilteredCorrelation;
-        
-        % Find peak of correlation map
-        [yPeak, xPeak] = find(gaussianDifferenceCorrelation==max(gaussianDifferenceCorrelation(:)));
-        
-        % If there is a tie for max peak, choose the one closest to the
-        % center of the correlation map.
-        if size(xPeak,1) > 1 && size(yPeak,1) > 1
-            indexOfClosest = 1;
-            closestDistance = Inf;
-            for i = 1:size(xPeak)
-                dist = (yPeak-size(correlationMap,1))^2 + (xPeak-size(correlationMap,2))^2;
-                if dist < closestDistance
-                    closestDistance = dist;
-                    indexOfClosest = i;
-                end
-            end
+if enableGaussianFiltering
+    % Apply Gaussian Filter and get difference between correlations
+    gaussianFilteredCorrelation = ...
+        imgaussfilt(correlationMap, ...
+        parametersStructure.gaussianStandardDeviation);
+    gaussianDifferenceCorrelation = ...
+        correlationMap - gaussianFilteredCorrelation;
 
-            xPeak = xPeak(indexOfClosest);
-            yPeak = yPeak(indexOfClosest);
-        end
-            
-        peakValue = correlationMap(yPeak, xPeak);
-        
-        % We do not use the second peak when using Gaussian approach
-        % because we do not care about peak ratio in this case.
-        secondPeakValue = NaN;
-        
-    else
-        % Find peak of correlation map
-        [yPeak, xPeak] = find(correlationMap==max(correlationMap(:)));
-        
-        % If there is a tie for max peak, arbitrarily break it since the
-        % second highest peak will be the same and this frame will be
-        % thrown out anyways.
-        if size(xPeak,1) > 1
-           xPeak = xPeak(1);
-        end
-        if size(yPeak,1) > 1
-            yPeak = yPeak(1);
-        end
-            
-        peakValue = correlationMap(yPeak, xPeak);
+    % Find peak of correlation map
+    [yPeak, xPeak] = find(gaussianDifferenceCorrelation==max(gaussianDifferenceCorrelation(:)));
 
-        % Find second highest point of correlation map
-        peakWindowMinX = max(1, xPeak - peakDropWindow);
-        peakWindowMaxX = min(size(correlationMap, 2), xPeak + peakDropWindow);
-        peakWindowMinY = max(1, yPeak - peakDropWindow);
-        peakWindowMaxY = min(size(correlationMap, 1), yPeak + peakDropWindow);
-        for y = (peakWindowMinY:peakWindowMaxY)
-            for x = (peakWindowMinX:peakWindowMaxX)
-                correlationMap(y, x) = -inf; % remove highest peak
+    % If there is a tie for max peak, choose the one closest to the
+    % center of the correlation map.
+    if size(xPeak,1) > 1 && size(yPeak,1) > 1
+        indexOfClosest = 1;
+        closestDistance = Inf;
+        for i = 1:size(xPeak)
+            dist = (yPeak-size(correlationMap,1))^2 + (xPeak-size(correlationMap,2))^2;
+            if dist < closestDistance
+                closestDistance = dist;
+                indexOfClosest = i;
             end
         end
-        secondPeakValue = max(correlationMap(:));
+
+        xPeak = xPeak(indexOfClosest);
+        yPeak = yPeak(indexOfClosest);
     end
+
+    peakValue = correlationMap(yPeak, xPeak);
+
+    % We do not use the second peak when using Gaussian approach
+    % because we do not care about peak ratio in this case.
+    secondPeakValue = NaN;
+
+else
+    % Find peak of correlation map
+    [yPeak, xPeak] = find(correlationMap==max(correlationMap(:)));
+
+    % If there is a tie for max peak, arbitrarily break it since the
+    % second highest peak will be the same and this frame will be
+    % thrown out anyways.
+    if size(xPeak,1) > 1
+       xPeak = xPeak(1);
+    end
+    if size(yPeak,1) > 1
+        yPeak = yPeak(1);
+    end
+
+    peakValue = correlationMap(yPeak, xPeak);
+
+    % Find second highest point of correlation map
+    peakWindowMinX = max(1, xPeak - peakDropWindow);
+    peakWindowMaxX = min(size(correlationMap, 2), xPeak + peakDropWindow);
+    peakWindowMinY = max(1, yPeak - peakDropWindow);
+    peakWindowMaxY = min(size(correlationMap, 1), yPeak + peakDropWindow);
+    for y = (peakWindowMinY:peakWindowMaxY)
+        for x = (peakWindowMinX:peakWindowMaxX)
+            correlationMap(y, x) = -inf; % remove highest peak
+        end
+    end
+    secondPeakValue = max(correlationMap(:));
+end
     
-    if isfield(parametersStructure, 'searchWindowPercentage')
-       % Add back in the offset removed in the beginning if applicable.
-       xPeak = xPeak + offset;
-       
-       % If results using this searchWindowPercentage are not acceptable,
-       % try again with a larger searchWindow.
-       isAcceptable = true;
-       if peakValue < parametersStructure.minimumPeakThreshold
+if isfield(parametersStructure, 'searchWindowPercentage')
+   % Add back in the offset removed in the beginning if applicable.
+   xPeak = xPeak + offset;
+
+   % If results using this searchWindowPercentage are not acceptable,
+   % try again with a larger searchWindow.
+   isAcceptable = true;
+   if peakValue < parametersStructure.minimumPeakThreshold
+       isAcceptable = false;
+    end
+    if parametersStructure.enableGaussianFiltering
+        % Fit a gaussian in a pixel window around the identified peak.
+        % The pixel window is of size
+        % |parametersStructure.SDWindowSize| x
+        % |parametersStructure.SDWindowSize/2|
+        %
+        % Take the middle row and the middle column, and fit a one-dimensional
+        % gaussian to both in order to get the standard deviations.
+        % Store results in statisticsStructure for choosing bad frames
+        % later.
+
+        % Middle row SDs in column 1, Middle column SDs in column 2.
+        middleRow = ...
+            correlationMap(max(ceil(yPeak-parametersStructure.SDWindowSize/2), 1): ...
+            min(floor(yPeak+parametersStructure.SDWindowSize/2), size(correlationMap,1)), ...
+            floor(xPeak));
+        middleCol = ...
+            correlationMap(floor(yPeak), ...
+            max(ceil(xPeak-parametersStructure.SDWindowSize/2), 1): ...
+            min(floor(xPeak+parametersStructure.SDWindowSize/2), size(correlationMap,2)))';
+        fitOutput = fit(((1:size(middleRow,1))-ceil(size(middleRow,1)/2))', middleRow, 'gauss1');
+        if fitOutput.c1 > parametersStructure.maximumSD
+            isAcceptable = false;
+        end
+        fitOutput = fit(((1:size(middleCol,1))-ceil(size(middleCol,1)/2))', middleCol, 'gauss1');
+        if fitOutput.c1 > parametersStructure.maximumSD
+            isAcceptable = false;
+        end
+    else
+       % Check peak ratio if not using gaussian filtering.
+       if secondPeakValue/peakValue > parametersStructure.maximumPeakRatio
            isAcceptable = false;
        end
-       if parametersStructure.enableGaussianFiltering
-            % Fit a gaussian in a pixel window around the identified peak.
-            % The pixel window is of size
-            % |parametersStructure.SDWindowSize| x
-            % |parametersStructure.SDWindowSize/2|
-            %
-            % Take the middle row and the middle column, and fit a one-dimensional
-            % gaussian to both in order to get the standard deviations.
-            % Store results in statisticsStructure for choosing bad frames
-            % later.
-
-            % Middle row SDs in column 1, Middle column SDs in column 2.
-            middleRow = ...
-                correlationMap(max(ceil(yPeak-parametersStructure.SDWindowSize/2), 1): ...
-                min(floor(yPeak+parametersStructure.SDWindowSize/2), size(correlationMap,1)), ...
-                floor(xPeak));
-            middleCol = ...
-                correlationMap(floor(yPeak), ...
-                max(ceil(xPeak-parametersStructure.SDWindowSize/2), 1): ...
-                min(floor(xPeak+parametersStructure.SDWindowSize/2), size(correlationMap,2)))';
-            fitOutput = fit(((1:size(middleRow,1))-ceil(size(middleRow,1)/2))', middleRow, 'gauss1');
-            if fitOutput.c1 > parametersStructure.maximumSD
-                isAcceptable = false;
-            end
-            fitOutput = fit(((1:size(middleCol,1))-ceil(size(middleCol,1)/2))', middleCol, 'gauss1');
-            if fitOutput.c1 > parametersStructure.maximumSD
-                isAcceptable = false;
-            end
-       else
-           % Check peak ratio if not using gaussian filtering.
-           if secondPeakValue/peakValue > parametersStructure.maximumPeakRatio
-               isAcceptable = false;
-           end
-       end
-       
-       % If the above checks found that this result was not acceptable and
-       % we can expand the searchWindowPercentage, run FindPeak again with
-       % a larger searchWindowPercentage via recursion.
-       if parametersStructure.searchWindowPercentage < 1 && ~isAcceptable
-           largerSearchWindowParams = parametersStructure;
-           largerSearchWindowParams.searchWindowPercentage = ...
-               min(parametersStructure.searchWindowPercentage + 0.1, 1);
-           [xPeak, yPeak, peakValue, secondPeakValue] = ...
-               FindPeak(originalCorrelationMap, largerSearchWindowParams);
-       end
     end
+       
+   % If the above checks found that this result was not acceptable and
+   % we can expand the searchWindowPercentage, run FindPeak again with
+   % a larger searchWindowPercentage via recursion.
+   if parametersStructure.searchWindowPercentage < 1 && ~isAcceptable
+       largerSearchWindowParams = parametersStructure;
+       largerSearchWindowParams.searchWindowPercentage = ...
+           min(parametersStructure.searchWindowPercentage + 0.25, 1);
+       [xPeak, yPeak, peakValue, secondPeakValue] = ...
+           FindPeak(originalCorrelationMap, largerSearchWindowParams);
+   end
 end
-
+end
