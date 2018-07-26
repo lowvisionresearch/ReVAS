272be40cb3def9a4569e5b7b6215b2a75c464f06
mnagaoglu <mnagaoglu@gmail.com>
2018-07-26 01:52:52 -0700

    GUI positions and sizes added

--- util/gui_utils/UIConfigMaker.m
+++ util/gui_utils/UIConfigMaker.m
@@ -1,10 +1,29 @@
-function UIConfigMaker
+function [GUIposition, uiFontSize,...
+        uiTitleFontSize,revasColors] = UIConfigMaker
 
-revasOuterPosition = [ 0.2327 0.1714 0.5393 0.6867];
+% GUI sizes and positions
+GUIposition.revas = [0.2327    0.1714     0.5393     0.6867];
+GUIposition.bandFiltParameters = [0.4256    0.1895    0.2125    0.4410];
+GUIposition.coarseParameters = [0.4375    0.1552    0.2125    0.4410];
+GUIposition.trimParameters = [0.4256    0.2752    0.1976    0.4610];
+GUIposition.stripParameters = [0.0738    0.2248    0.3839    0.6210];
+GUIposition.stimParameters = [0.4214    0.1800    0.2214    0.5229];
+GUIposition.sacParameters = [0.1000    0.2819    0.3601    0.5162];
+GUIposition.filtParameters = [0.1030    0.1305    0.3554    0.6638];
+GUIposition.fineParameters = [0.0946    0.2114    0.3661    0.6190];
+GUIposition.rerefParameters = [0.6310    0.1514    0.2006    0.7790];
+GUIposition.parallelization = [0.6173    0.4190    0.1982    0.2705];
+GUIposition.gammaParameters = [0.4181    0.3143    0.2135    0.3486];
+
+
+% font sizes
 uiFontSize = 14;
 uiTitleFontSize = 18;
+
+
+% color theme
 revasColors.background = [255 255 255]/255;
-revasColors.boxBackground = [255 255 255]/255;
+revasColors.boxBackground = [245 245 245]/255;
 revasColors.text = [0 0 0]/255;
 revasColors.activeButtonBackground = [0 122 255]/255;
 revasColors.activeButtonText = [255 255 255]/255;
@@ -17,5 +36,6 @@ revasColors.passiveBorder = [142 142 147]/255;
 revasColors.abortButtonBackground = [255 59 48]/255;
 revasColors.abortButtonText = [255 255 255]/255;
 
-save([pwd '/gui/uiconfig.mat'],'revasOuterPosition','uiFontSize',...
+% save them in a config file
+save([pwd '/gui/uiconfig.mat'],'GUIposition','uiFontSize',...
     'uiTitleFontSize','revasColors');
