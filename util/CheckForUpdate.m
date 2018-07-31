31077746282fadc9ff2cfabeb4a19d9b2b8d7524
mnagaoglu <mnagaoglu@gmail.com>
2018-07-31 02:05:43 -0700

    new updater. Doesn't need an external website to hold the version number. Requires git. needs to be tested on Windows.

---
+++ util/CheckForUpdate.m
@@ -0,0 +1,38 @@
+function [isSuccess, msg] = CheckForUpdate
+
+% Check if there is a newer version
+try
+    [~, cmdout] = system('git fetch --dry-run');
+catch err0
+    msg = sprintf('"git fetch" failed.\n%s',err0.message);
+    isSuccess = 0;  
+    return;
+end
+
+if ~isempty(cmdout)
+    % Construct a questdlg with three options
+    choice = questdlg(['There is an update version of ReVAS.' ...
+        'Would you like to update now?'], ...
+        'Update available','Yes','No','Yes');
+    % Handle response
+    try
+        switch choice
+            case 'Yes'
+                !git pull
+                msg = 'ReVAS has been updated';
+            case 'No'
+                msg = 'User skipped the update.';
+            otherwise 
+                % do nothing
+        end
+    catch err
+        msg = sprintf('Update failed!\n%s',err.message);
+        isSuccess = 0;  
+        return;
+    end
+    
+else
+    msg = 'ReVAS is already up to date.';
+end
+
+isSuccess = 1;
