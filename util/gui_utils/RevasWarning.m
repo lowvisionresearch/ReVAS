35942bc928bfecaac771b80b6c8341a64dd8a8c2
mnagaoglu <mnagaoglu@gmail.com>
2018-11-21 00:26:09 -0800

    fixed the issue where nargin<2 would crash the function. also fixed the mistake to re-write the whole log everytime these functions are called

--- util/gui_utils/RevasWarning.m
+++ util/gui_utils/RevasWarning.m
@@ -2,12 +2,16 @@ function RevasWarning(message, parametersStructure)
 %REVAS WARNING  Issues a warning to the GUI's command window text box if possible.
 %   Issues a warning to the GUI's command window text box if possible.
 
-if isfield(parametersStructure, 'commandWindowHandle')
-    time = strtrim(datestr(datetime('now'), 'HH:MM:SS PM'));
-    parametersStructure.commandWindowHandle.String = ...
-        ['(' time ') Warning: ' ...
-        message; ...
-        parametersStructure.commandWindowHandle.String];
+time = strtrim(datestr(datetime('now'), 'HH:MM:SS PM'));
+
+% if available, print the message to REVAS GUI.
+if nargin > 1
+    if isfield(parametersStructure, 'commandWindowHandle')
+        parametersStructure.commandWindowHandle.String = ...
+            ['(' time ') Warning: ' ...
+            message; ...
+            parametersStructure.commandWindowHandle.String];
+    end
 end
 
 % get system color for text
@@ -15,8 +19,6 @@ c = com.mathworks.services.Prefs.getColorPref('Colors_M_Warnings');
 textColor = [get(c,'Red') get(c,'Green') get(c,'Blue')]/255;
 
 % display the same message via MATLAB command window
-for i=1:length(parametersStructure.commandWindowHandle.String)
-    cprintf(textColor,'%s\n',parametersStructure.commandWindowHandle.String{i});
-end
+cprintf(textColor,'%s\n',(['(' time ') WARNING: ' message]));
 
 end
