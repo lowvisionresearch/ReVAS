b4bfe4cf9b19be15879f957a764762d68ab0d94a
Matthew Sit <msit@berkeley.edu>
2019-08-19 14:10:48 -0700

    add preliminary timing tests

---
+++ testing/StripAnalysis_profile.m
@@ -0,0 +1,32 @@
+function StripAnalysis_profile
+% StripAnalysis_profile
+%  Runs the core strip analysis function on the pre-processed demo video,
+%  providing a timing profile for the current implementation.
+%
+%  MTS 8/19/19 wrote the initial version
+
+addpath(genpath('..'))
+clc
+
+parametersStructure = struct;
+parametersStructure.overwrite = true;
+
+global abortTriggered;
+abortTriggered = 0;
+
+warning('off','all')
+profile on
+
+[rawEyePositionTraces, usefulEyePositionTraces, timeArray, ...
+    statisticsStructure] = StripAnalysis( ...
+    'demo/sample10deg_dwt_nostim_gamscaled_bandfilt.avi', ...
+    'demo/sample10deg_dwt_nostim_gamscaled_bandfilt_refframe.mat', ...
+    parametersStructure ...
+);
+
+profile viewer
+warning('on','all')
+
+% profsave
+profile off
+end
